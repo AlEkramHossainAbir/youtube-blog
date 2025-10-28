915691b1588eab3f70a68ad93e1868436b5c31a6
Al- Ekram Hossain Abir <alabir65@gmail.com>
2025-10-28 10:21:00 +0600

    add hash password

--- models/user.js
+++ models/user.js
@@ -1,46 +1,65 @@
-const mongoose = require('mongoose');
-const path = require('path');
+const mongoose = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    lastName:{
-        type: String,
-        required: true
+    lastName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    salt:{
-        type: String,
-        required: true,
+    salt: {
+      type: String,
+      required: true,
     },
-    password:{
-        type: String,
-        required: true,
+    password: {
+      type: String,
+      required: true,
     },
-    profileImageUrl:{
-        type: String,
-        default: "/images/default.png",
+    profileImageUrl: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-    },
-    role:{
-        type: String,
-        enum:["USER", "ADMIN"],
-        default: "USER"
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
-},{
-    timestamps: true
-})
+userSchema.pre("save", function (next) {
+  const user = this;
 
-const user = mongoose.model('user', userSchema)
+  if (!user.isModified("password")) return next();
 
-module.exports = {user}
+  const salt = randomBytes(16).toString();
+
+  const hashPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.password = hashPassword;
+  this.salt = salt;
+
+  next();
+});
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = { user };
