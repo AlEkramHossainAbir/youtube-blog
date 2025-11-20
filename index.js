e1e2d4890aa3e5999e7dbb5e8556c1d439063176
Al Ekram Hossain Abir <alabir65@gmail.com>
2025-11-20 21:30:13 +0600

    wip with middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express= require("express");
 const path = require("path")
 const userRoute = require("./router/user")
 const {connectDB} = require("./connect")
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express()
 
@@ -12,11 +14,15 @@ connectDB('mongodb://127.0.0.1:27017/blogify')
 app.set("view engine", "ejs");
 app.set("views",path.resolve("./views"))
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie("token"))
 
 app.use("/user",userRoute)
 
 app.get("/", (req, res)=>{
-    res.render("home");
+    res.render("home",{
+        user: req.user
+    });
 })
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,13 +1,22 @@
-function checkForAuthenticationCookie(cookieName){
-    return (req,res,next)=>{
-        const tokenCookieValue = req.cookies[cookieName];
-        if(!tokenCookieValue){
-            return res.redirect("/user/signin");
-        }
-        next();
+const { validateToken } = require("../services/authentication");
 
+function checkForAuthenticationCookie(cookieName) {
+    console.log("Authentication middleware loaded",cookieName);
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+        next()
     }
-
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+      next()
+    } catch (err) {
+        res.status(401).send('Invalid token');
+    return;
+}
+// next()
+  };
 }
 
-module.exports = {checkForAuthenticationCookie}
+module.exports = { checkForAuthenticationCookie };
